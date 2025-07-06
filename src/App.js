8177d20ea18a80b35625d92f5ded4dc280644fcf
olflin <raymond.13300@gmail.com>
2025-07-06 10:13:39 +0200

    ajout fichier et folder ainsi que mise en place du rooter react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import react from "react";
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+
+
+import Home from './pages/Home';
+import Contact from './pages/Contact';
+import Mentions from './pages/Mentions';
+import Portfolio from './pages/Portfolio';
+import Services from './pages/Services';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+    return (
+        <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/Contact" element={<Contact/>}></Route>
+            <Route path="/Mentions" element={<Mentions/>}></Route>
+            <Route path="/Portfolio" element={<Portfolio/>}></Route>
+            <Route path="/Services" element={<Services/>}></Route>
+        </Routes>
+    );
+    }
