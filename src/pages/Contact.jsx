2d7646d6dc11e6c4f4160b1121b982e3c441a41a
olflin <raymond.13300@gmail.com>
2025-07-08 09:53:41 +0200

    ajout page Contact.jsx et Contact.css

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,57 @@
-
+import { Container, Row, Col, Form, Button } from 'react-bootstrap';
+import '../assets/styles/Contact.css';
 
 
 function Contact() {
+      return (
+        <Container className="my-5">
+              <h2 className="text-center fw-bold mb-2">Contact</h2>
+              <p className="text-center mb-4">
+                Pour me contacter en vue d’un entretien ou d’une future collaboration, merci de remplir le formulaire de contact.
+              </p>
+            <div className='section-contact border-bottom border-primary border-5 mb-4 pb-3'>
+            </div>
 
-    
+          {/* Formulaire et Coordonnées */}
+          <Row className="shadow rounded p-4 bg-white mt-5">
+            {/* Formulaire */}
+            <Col md={6} className="mb-4 mb-md-0">
+              <h3 className="fw-bold border-bottom border-primary border-3 mb-5">Formulaire de contact</h3>
+              <Form>
+                <Form.Control className="mb-2" type="text" placeholder="Votre nom" />
+                <Form.Control className="mb-2" type="email" placeholder="Votre adresse email" />
+                <Form.Control className="mb-2" type="tel" placeholder="Votre numéro de téléphone" />
+                <Form.Control className="mb-2" type="text" placeholder="Sujet" />
+                <Form.Control className="mb-3" as="textarea" rows={5} placeholder="Votre message" style={{ height: '500px' }} />
+                <Button variant="primary" type="submit">Envoyer</Button>
+              </Form>
+            </Col>
+
+            {/* Coordonnées */}
+            <Col md={6}>
+              <h3 className="fw-bold border-bottom border-primary border-3 mb-5">Mes coordonnées</h3>
+              <p className='fw-bold'>John Doe</p>
+              <p><i className="bi bi-building"></i> 40 rue Laure Diebold</p>
+              <p><i className="bi bi-geo-alt"></i> 69009 Lyon, France</p>
+              <p><i className="bi bi-telephone"></i> 10 20 30 40 50</p>
+              <p><i className="bi bi-envelope"></i> john.doe@gmail.com</p>
+
+              {/* Google Maps iframe */}
+              <div className="mt-3">
+                <iframe
+                  title="Localisation"
+                  src="https://www.google.com/maps?q=40+rue+Laure+Diebold,+Lyon&output=embed"
+                  width="100%"
+                  height="250"
+                  style={{ border: 0,  height: '500px'}}
+                  allowFullScreen=""
+                  loading="lazy"
+                ></iframe>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+  );
 }
+
 export default Contact;
